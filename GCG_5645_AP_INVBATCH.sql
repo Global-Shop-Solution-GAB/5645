745fb29eed8096e2cb9075308118a5ffabe9383a
Service Web <sw@gssmail.com>
2024-03-14 13:54:14 -0500

    Commit changes in C:\Websites\Git\emarlow\5645\GCG_5645_AP_INVBATCH.sql

---
+++ GCG_5645_AP_INVBATCH.sql
@@ -0,0 +1,10 @@
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_5645_AP_INVBATCH" (
+ "AP_BATCH" CHAR(5),
+ "AP_INV" CHAR(7),
+ "AP_YEAR" CHAR(4) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
