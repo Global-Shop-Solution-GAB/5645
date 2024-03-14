8ab6a42ed5f83e924f55823c9619c19813285635
Service Web <sw@gssmail.com>
2024-03-14 13:54:14 -0500

    Commit changes in C:\Websites\Git\emarlow\5645\GCG_5645_OVR_CUST_RT.sql

---
+++ GCG_5645_OVR_CUST_RT.sql
@@ -0,0 +1,12 @@
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_5645_OVR_CUST_RT" (
+ "CUST_ID" CHAR(6),
+ "CUST_RATE" NUMERIC(6,4),
+ "GSS_USER" CHAR(9),
+ "GSS_DATE" CHAR(10),
+ "CUST_SP_ID" CHAR(3) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
